cdd5a191087d6341de5a43e203c250822343df82
Mahdi Saedi <mahdiisaedii@gmail.com>
2021-04-17 12:34:09 +0430

    Create Avocados Project

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by MahDi SaeDi on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
